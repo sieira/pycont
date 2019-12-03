5f37b001dc28c429dee36d18e7f1296d4a28e68c
Luis Sieira <luis.sieira@spacebar.fr>
2019-12-03 22:46:23 +0100

    Add test coverage for UI

--- ui/src/App.test.tsx
+++ ui/src/App.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureMockStore from "redux-mock-store";
+import thunkMiddleware from 'redux-thunk-recursion-detect';
+
 import App from './App';
+
+const mockStore = configureMockStore([thunkMiddleware]);
+const store = mockStore({});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>, div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
