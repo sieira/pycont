567ab41222d47afabb79bb3aefec65de4722a215
Luis Sieira <luis.sieira@spacebar.fr>
2019-12-04 20:56:17 +0100

    Increase coverage

--- ui/src/components/login/login.test.tsx
+++ ui/src/components/login/login.test.tsx
@@ -4,13 +4,35 @@ import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
 import thunkMiddleware from 'redux-thunk-recursion-detect';
 
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import Login from '.';
 
 const mockStore = configureMockStore([thunkMiddleware]);
-const store = mockStore({});
+const store = mockStore({})
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+configure({adapter: new Adapter()});
+
+it('should render without crashing', () => {
   const loginForm = create(<Provider store={store}><Login /></Provider>);
   expect(loginForm.toJSON()).toMatchSnapshot();
+});
+
+it('should be disabled when no content', () => {
+  const loginForm = create(<Provider store={store}><Login /></Provider>).root;
+  const loginButton = loginForm.findByType('button');
+  expect(loginButton).toBeDefined();
+  expect(loginButton.props.disabled).toBe(true);
+});
+
+it('should login when required', () => {
+  const loginForm = mount(<Provider store={store}><Login /></Provider>);
+  const loginButton = loginForm.find('button');
+  const usernameText = loginForm.find('input[type="username"]');
+  const passwordText = loginForm.find('input[type="password"]');
+  usernameText.simulate('change', {target: {value: 'Bazinga'}});
+  passwordText.simulate('change', {target: {value: 'Spontiak!!'}});
+  const loginButtonAfter = loginForm.find('button');
+  expect(loginButtonAfter.props().disabled).toBe(false);
 });
