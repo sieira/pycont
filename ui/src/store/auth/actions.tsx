e633ba17632d95e29f02bce8148061440b49c4dc
Luis Sieira <luis.sieira@sap.com>
2019-12-26 10:20:25 +0100

    {refreshAuth} Use the refresh token

--- ui/src/store/auth/actions.tsx
+++ ui/src/store/auth/actions.tsx
@@ -70,6 +70,28 @@ export function login(username: string, password: string) {
   }
 }
 
+export function refreshAuth() {
+  return async (
+    dispatch: Dispatch<AuthenticationAction, {}, Action>
+  ): Promise<void> => {
+    return fetch('api/auth/refresh/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+    }).then(function(response) {
+      if (response.status !== 200) {
+        dispatch(logout())
+      } else {
+        response.json().then(data => {
+          dispatch(authenticate(data.user))
+        })
+      }
+    })
+  }
+}
+
 export function checkAuth() {
   return async (
     dispatch: Dispatch<AuthenticationAction, {}, Action>
@@ -82,8 +104,7 @@ export function checkAuth() {
       method: 'GET'
     }).then(function(resp) {
       if (resp.status !== 200) {
-        dispatch(unauthenticate())
-        return
+        dispatch(refreshAuth())
       } else {
         resp.json().then(data => dispatch(authenticate(data)))
       }
